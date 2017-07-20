95862d593a2dc9f994ac22816f4ec524456cd363
Andrius Mikalauskas <amikalauskas@081.k4.nfq.lt>
2017-07-20 12:03:06 +0300

    basic init

---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getTodos } from '@/mocks/Todo'
+
+export default {
+  mounted () {
+    this.loadTodos()
+  },
+  data () {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    loadTodos () {
+      getTodos().then((response) => {
+        this.todos = response
+      })
+    }
+  }
+}
+</script>
